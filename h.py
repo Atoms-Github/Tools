329a7f9ec35aeed86e7ef33dca0b4306877510ef
Thomas Mullender <tomullender@gmail.com>
2022-08-16 14:09:53 +0100

    Added t shortcut thing

---
+++ h.py
@@ -0,0 +1,32 @@
+import datetime
+import webbrowser
+import os
+import subprocess
+import time
+import config
+
+reddit = "https://www.reddit.com/r/dailyprogrammer/wiki/challenges/"
+spreadsheet = "https://docs.google.com/spreadsheets/d/1aexPbZMsa3SKDMD6JZX3xv6j7ieiaxKV62vwMNfvfwk/edit#gid=0"
+month = r"C:\_C_\08-22 - August\Daily"
+
+def main():
+    month = config.get("month")
+    day = datetime.datetime.today().day
+    subdir = month + "\\" + "a" + str(day)
+    webbrowser.open(reddit)
+
+    webbrowser.open(spreadsheet)
+    # Get current date of the month
+    try:
+        os.makedirs(subdir)
+        subprocess.call(["cargo.exe", "init"], cwd=subdir)
+
+    except FileExistsError:
+        pass
+    time.sleep(1)
+    subprocess.call(["clion64.exe", '"' + subdir + '"'], cwd=subdir)
+    # Use subprocess to run cargo.exe in the subdirectory
+
+
+if __name__ == "__main__":
+    main()
--- t.py
+++ t.py
@@ -1,29 +1,7 @@
-import datetime
-import webbrowser
 import os
-import subprocess
-import time
-
-reddit = "https://www.reddit.com/r/dailyprogrammer/wiki/challenges/"
-spreadsheet = "https://docs.google.com/spreadsheets/d/1aexPbZMsa3SKDMD6JZX3xv6j7ieiaxKV62vwMNfvfwk/edit#gid=0"
-daily = r"C:\_C_\08-22 - August\Daily"
-def main():
-    day = datetime.datetime.today().day
-    subdir = daily + "\\" + "a" + str(day)
-    webbrowser.open(reddit)
-
-    webbrowser.open(spreadsheet)
-    # Get current date of the month
-    try:
-        os.makedirs(subdir)
-        subprocess.call(["cargo.exe", "init"], cwd=subdir)
-
-    except FileExistsError:
-        pass
-    time.sleep(1)
-    subprocess.call(["clion64.exe", '"' + subdir + '"'], cwd=subdir)
-    # Use subprocess to run cargo.exe in the subdirectory
-
+import config
 
 if __name__ == "__main__":
-    main()
+    month = config.get("month")
+    os.chdir(month)
+    os.system(f"cmd.exe")
