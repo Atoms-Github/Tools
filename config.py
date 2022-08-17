f1aab11b8bdfd37025ac8922c9274fd0993bfe06
AtomsPC <tomullender@gmail.com>
2022-08-17 11:50:27 +0100

    Added default_config.toml

--- config.py
+++ config.py
@@ -2,25 +2,24 @@ import toml
 import os
 
 
-file_name = "config.toml"
+config_filename = "config.toml"
+default_cfg_filename = "default_config.toml"
 def get(name):
     gen()
-    with open(file_name, "r") as toml_file:
+    with open(config_filename, "r") as toml_file:
         data = toml.load(toml_file)
     return data[name]
+def get_default_cfg():
+    with open(default_cfg_filename, "r") as toml_file:
+        data = toml.load(toml_file)
+    return data
 
 def gen():
-    if os.path.isfile(file_name):
+    if os.path.isfile(config_filename):
         return
-    di = {
-        "month": "C:\\__E__\\2022\\August",
-        "os": {
-            "os": "win 10",
-            "Arch": "x64"
-        },
-    }
+    di = get_default_cfg()
 
-    with open(file_name, "w") as toml_file:
+    with open(config_filename, "w") as toml_file:
         toml.dump(di, toml_file)
 
 if __name__ == "__main__":
