7bad04ecd96ac0dba9f9c53034c65336a0369012
Thomas <tomullender@gmail.com>
2022-10-29 16:38:11 +0100

    Removed websites

--- h.py
+++ h.py
@@ -5,14 +5,10 @@ import subprocess
 import time
 import config
 
-reddit = "https://leetcode.com/"
 
 def main():
     day = datetime.datetime.today().day
     subdir = os.path.expandvars("%month%") + "\\Daily\\" + "a" + str(day)
-    webbrowser.open(reddit)
-
-    webbrowser.open(spreadsheet)
     try:
         os.makedirs(subdir)
         subprocess.call(["cargo.exe", "init"], cwd=subdir)
